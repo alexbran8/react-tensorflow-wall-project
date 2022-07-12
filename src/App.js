06ad1ca0843c0d5f8b7e64fff01e9b7dd4bd3500
abran <alexandru.bran@nokia.com>
2022-07-12 10:07:15 +0300

    webcam 1st test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { WebcamCapture} from './components/webcam/webcam'
+import {isMobile} from 'react-device-detect';
+
 
 function App() {
+  // const classes = useStyles();
+
+
+  const [source, setSource] = useState("");
+
+
+
+  ;const handleCapture = (target) => {
+    if (target.files) {
+      if (target.files.length !== 0) {
+        const file = target.files[0];
+        const newUrl = URL.createObjectURL(file);
+        setSource(newUrl);
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+          <h5>Capture your image</h5>
+          {isMobile ? <h5>Loaded from mobile</h5>:<h5>Loaded from desktop</h5>}
+          {/* { source && <img src={source} alt={"snap"} ></img> } */}
+          <WebcamCapture/>
+          <input
+            accept="image/*"
+            // className={classes.input}
+            id="icon-button-file"
+            type="file"
+            capture="environment"
+            onChange={(e) => handleCapture(e.target)}
+          />
     </div>
   );
 }
+
 
 export default App;
