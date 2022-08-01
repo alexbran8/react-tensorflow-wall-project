356925dde0512c58904cb386c039fda9f55c535e
abran <alexandru.bran@nokia.com>
2022-08-01 11:08:40 +0300

    added first tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen, act, fireEvent } from '@testing-library/react'
+import Home from './components/Home'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders a title', () => {
+  const { getByText } = render(<Home />)
+  const title = "This application works in desktop mode"
+  expect(getByText(title)).toBeInTheDocument()
+})
+
+test('renders a responsive title', () => {
+  const { rerender, container } = render(<Home />)
+  let title = "Start Detect"
+  expect(container).toHaveTextContent(title)
+  act(() => {
+    window.innerWidth = 1000
+    fireEvent(window, new Event('resize'))
+  })
+  rerender(<Home />)
+  expect(screen.getByText('Start Detect')).toBeVisible()
+})
